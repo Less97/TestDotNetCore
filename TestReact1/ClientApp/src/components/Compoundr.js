2d38f3f0f13e4078a6d072410cb0896af16b77c4
Alessandro Orlandi <aorlandi@mi9retail.com>
2019-11-04 15:13:13 +0000

    [Working] - Working on dropdownlist

--- TestReact1/ClientApp/src/components/Compoundr.js
+++ TestReact1/ClientApp/src/components/Compoundr.js
@@ -5,25 +5,55 @@ import { actionCreators } from '../store/Compoundr';
 
 class Compoundr extends Component {
 
-    handleChange(initialAmount, interest, years) {
+    handleChange(initialAmount, interest,frequency,years) {
         this.props.updateForm(initialAmount, interest, years);
     }
 
-    compound() {
+    compound(event) {
+        event.preventDefault();
         this.props.compound();
     }
 
     render() {
         return (
             <div>
-                <label htmlFor="initialAmount">initial amount:</label><input name="initialAmount" type="text" value={this.props.initialAmount} onChange={(event) => this.handleChange(event.target.value, this.props.interest, this.props.years)} /><br /><br />
-                <label htmlFor="years">years:</label><input name="years" type="text" value={this.props.years} onChange={(event) => this.handleChange(this.props.initialAmount, this.props.years, event.target.value)} /><br /><br />
-                <label htmlFor="interest">interest:</label><input name="interest" type="text" value={this.props.interest} onChange={(event) => this.handleChange(this.props.initialAmount, event.target.value, this.props.years)} /><br /><br />
-                <button className="btn btn-primary" onClick={this.props.compound}>COMPOUND</button>
-                <br /><br/>
-                <label>{this.props.endEmount}</label>
-        </div>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="initialAmount">Initial amount:</label>
+                    <input name="initialAmount" type="text" value={this.props.initialAmount} onChange={(event) => this.handleChange(event.target.value, this.props.interest, this.props.years)} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="years">Years:</label>
+                    <input name="years" type="text" value={this.props.years} onChange={(event) => this.handleChange(this.props.initialAmount, this.props.years, event.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="interest">Interest:</label>
+                    <input name="interest" type="text" value={this.props.interest} onChange={(event) => this.handleChange(this.props.initialAmount, event.target.value, this.props.years)} />
+                    </div>
+                    <div>
+                        <label htmlFor="frequency">Compounding periods</label>
+                        <select name="frequency" onChange={() => this.dropdownlistChange}>
+                            <option value="monthly">Monthly</option>
+                            <option value="yearly">Yearly</option>
+                        </select>    
+                    </div>
+                <div className="form-group">
+                    <label htmlFor="interest">added monthly:</label>
+                    <input name="monthlyAdd" type="text" value={this.props.interest} onChange={(event) => this.handleChange(this.props.initialAmount, event.target.value, this.props.years)} />
+                </div>
+                <button className="btn btn-primary" onClick={event=>this.compound(event)}>COMPOUND</button>
+                </form>
+                <div>{this.renderResult()}</div>
+            </div>
     );
+    }
+
+    renderResult() {
+        return (
+            <div>
+                {this.props.endAmount}
+            </div>)
+
     }
 }
 
--- TestReact1/ClientApp/src/store/Compoundr.js
+++ TestReact1/ClientApp/src/store/Compoundr.js
@@ -5,7 +5,9 @@ const initialState = {
     initialAmount: 0,
     interest:0,
     years: 0,
-    endEmount: 0
+    frequency:'monthly',
+    monthlyAdd:0,
+    endAmount: 0
 };
 
 export const actionCreators = {
@@ -18,12 +20,9 @@ export const reducer = (state, action) => {
 
     switch (action.type) {
         case OPERATION_COMPOUND:
-
-            return { ...state, endEmount: calculateCompoundInterest(state.initialAmount, state.years, state.interest) };
-
+            return { ...state, endAmount: calculateCompoundInterest(state.initialAmount, state.years, state.interest) };
         case OPERATION_UPDATE:
             return { ...state, initialAmount: action.initialamount, interest: action.interest, years: action.years }
-
         default: 
             return state;
     }
