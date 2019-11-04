e631d46f3c4ff7211c81b8bc13208175bb97ed00
Alessandro Orlandi <aorlandi@mi9retail.com>
2019-11-04 11:39:41 +0000

    [Enhancement] - Form is working

--- TestReact1/ClientApp/src/components/Compoundr.js
+++ TestReact1/ClientApp/src/components/Compoundr.js
@@ -5,8 +5,8 @@ import { actionCreators } from '../store/Compoundr';
 
 class Compoundr extends Component {
 
-    handleChange(a,b) {
-        this.props.updateForm(a, b);
+    handleChange(initialAmount, interest, years) {
+        this.props.updateForm(initialAmount, interest, years);
     }
 
     compound() {
@@ -16,11 +16,12 @@ class Compoundr extends Component {
     render() {
         return (
             <div>
-                <label for="initialAmount">initial amount:</label><input name="initialAmount" type="text" value={this.props.a} onChange={(event) => this.handleChange(event.target.value, this.props.b)} /><br /><br />
-                <label for="initialAmount">years:</label><input type="text" value={this.props.b} onChange={(event) => this.handleChange(this.props.a,event.target.value)} /><br /><br />
+                <label htmlFor="initialAmount">initial amount:</label><input name="initialAmount" type="text" value={this.props.initialAmount} onChange={(event) => this.handleChange(event.target.value, this.props.interest, this.props.years)} /><br /><br />
+                <label htmlFor="years">years:</label><input name="years" type="text" value={this.props.years} onChange={(event) => this.handleChange(this.props.initialAmount, this.props.years, event.target.value)} /><br /><br />
+                <label htmlFor="interest">interest:</label><input name="interest" type="text" value={this.props.interest} onChange={(event) => this.handleChange(this.props.initialAmount, event.target.value, this.props.years)} /><br /><br />
                 <button className="btn btn-primary" onClick={this.props.compound}>COMPOUND</button>
                 <br /><br/>
-                <label>{this.props.s}</label>
+                <label>{this.props.endEmount}</label>
         </div>
     );
     }
--- TestReact1/ClientApp/src/store/Compoundr.js
+++ TestReact1/ClientApp/src/store/Compoundr.js
@@ -1,15 +1,16 @@
 const OPERATION_COMPOUND = 'SUM'
 const OPERATION_UPDATE = 'UPDATE'
+
 const initialState = {
     initialAmount: 0,
+    interest:0,
     years: 0,
-    compounded: 0
+    endEmount: 0
 };
 
 export const actionCreators = {
     compound: () => ({ type: OPERATION_COMPOUND }),
-    updateForm: (a, b) =>  ({ type: OPERATION_UPDATE, a, b })
-    
+    updateForm: (initialamount, interest, years)  => ({ type: OPERATION_UPDATE, initialamount, interest, years })
 };
 
 export const reducer = (state, action) => {
@@ -17,14 +18,20 @@ export const reducer = (state, action) => {
 
     switch (action.type) {
         case OPERATION_COMPOUND:
-           
-            return { ...state, compoundrd: 100 };
+
+            return { ...state, endEmount: calculateCompoundInterest(state.initialAmount, state.years, state.interest) };
 
         case OPERATION_UPDATE:
-            return { ...state, a: action.a, b: action.b }
+            return { ...state, initialAmount: action.initialamount, interest: action.interest, years: action.years }
 
         default: 
             return state;
     }
 
 };
+
+function calculateCompoundInterest(initialAmount, years, interest) {
+    initialAmount = parseInt(initialAmount, 10); years = parseInt(years, 10); interest = (parseInt(interest, 10)) / 100;
+    var endAmount = (initialAmount * Math.pow((1 + (interest / years)), years)).toFixed(2);
+    return endAmount;
+}
